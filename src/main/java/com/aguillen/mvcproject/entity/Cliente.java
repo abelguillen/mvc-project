6b52e96a75d7b60b1548c0a3b35709d96685fd59
NaraLarrosa <naranatalia.larrosa@gmail.com>
2023-08-29 20:04:53 -0300

    se agrega abm de cliente

---
+++ src/main/java/com/aguillen/mvcproject/entity/Cliente.java
@@ -0,0 +1,68 @@
+package com.aguillen.mvcproject.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nombre;
+	
+	private String apellido;
+	
+	private String direccion;
+	
+	public Cliente() {}
+
+	public Cliente(Long id, String nombre, String apellido, String direccion) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.direccion = direccion;
+	}
+
+	public Cliente(String nombre, String apellido, String direccion) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.direccion = direccion;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+}
