ea711f07d2c9ae03a953c5ef36e147327f1b928f
abelguillen <abel.guillen@hotmail.com>
2023-09-06 15:53:47 -0300

    add update and delete

--- src/main/java/com/aguillen/mvcproject/controller/ClienteController.java
+++ src/main/java/com/aguillen/mvcproject/controller/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -52,5 +53,35 @@ public class ClienteController {
 		}
 		return "redirect:" + "/clientes/lista";
 	}
-
+	
+	@PostMapping("/actualizar")
+	public String actualizarPut(
+			@RequestParam(value = "id") Long id,
+			@RequestParam(value = "nombre") String nombre,
+			@RequestParam(value = "apellido") String apellido,
+			@RequestParam(value = "direccion") String direccion) {
+		try {
+			repository.save(new Cliente(id, nombre, apellido, direccion));
+		} catch(Exception ex) {
+			System.out.println("Ha ocurrido un error al intentar actualizar un cliente en la base de datos: " + ex.getMessage());
+		}
+		return "redirect:" + "/clientes/lista";
+	}
+	
+	@GetMapping("/actualizar/{id}")
+	public String actualizar(@PathVariable(value = "id") Long id, Model template) {
+		Cliente cliente = repository.findById(id).get();
+		template.addAttribute("cliente", cliente);
+		return "clientes_actualizar";
+	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String eliminar(@PathVariable(value = "id") Long id) {
+		try {
+			repository.deleteById(id);
+		} catch(Exception ex) {
+			System.out.println("Ha ocurrido un error al intentar eliminar un cliente en la base de datos: " + ex.getMessage());
+		}
+		return "redirect:" + "/clientes/lista";
+	}
 }
