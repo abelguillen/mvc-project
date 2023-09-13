4e29acb47d9e355c9f75830fab906ad186e67378
abelguillen <abel.guillen@hotmail.com>
2023-09-13 17:17:19 -0300

    added customer finder

--- src/main/java/com/aguillen/mvcproject/repository/ClienteRepository.java
+++ src/main/java/com/aguillen/mvcproject/repository/ClienteRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.aguillen.mvcproject.entity.Cliente;
+import com.aguillen.mvcproject.entity.Persona;
 
 @Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Long> {
-
+	public Cliente findByNombre(String nombre);
+	public Cliente findByApellido(String apellido);
 }
